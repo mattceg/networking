34b0b420a002d7dcb70137e15c820973fc224cfb
Matt S <mattceg@gmail.com>
2021-02-25 22:33:24 +0000

    Added subnets in vpc

--- networking.tf
+++ networking.tf
@@ -4,3 +4,13 @@ resource "google_compute_network" "vpc"{
     name = "matt-03-vpc"
     auto_create_subnetworks = "false"
 }
+ 
+//Create Subnet
+
+resource "google_compute_subnetwork" "subnet"{
+     name = "matt-03-subnet"
+     ip_cidr_range = "10.10.3.0/24"
+     network = google_compute_network.vpc.name
+     depends_on = [google_compute_network.vpc]
+     region = "us-central1"
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
 output "matts-vpc"{
   value = google_compute_network.vpc.id
 }
+output "matts-subnet"{
+ value = google_compute_subnetwork.subnet.id
+}
